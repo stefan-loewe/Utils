e3fd51e9658b4a80eae9621fd2d3f12272f736fb
Stefan Löwe <stefan@loewe.ws>
2012-10-29 21:54:22 +0100

    updated to match current coding style

--- src/ws/loewe/Utils/File/IFileSystemObject.php
+++ src/ws/loewe/Utils/File/IFileSystemObject.php
@@ -5,6 +5,5 @@ namespace ws\loewe\Utils\File;
 /**
  * This interface defines a type for representing an abstract object in the file system.
  */
-interface IFileSystemObject
-{
+interface IFileSystemObject {
 }
