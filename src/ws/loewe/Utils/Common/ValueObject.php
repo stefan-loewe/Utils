7d846d84aa2f3378f0398b36c7a9db77ee9b50a1
Stefan Löwe <loewe@fim.uni-passau.de>
2014-09-04 20:39:50 +0200

    fix echo

--- src/ws/loewe/Utils/Common/ValueObject.php
+++ src/ws/loewe/Utils/Common/ValueObject.php
@@ -4,7 +4,6 @@ namespace ws\loewe\Utils\Common;
 
 trait ValueObject {
     public function __get($memberName) {
-      echo 123;
       if(!property_exists($this, $memberName)) {
         throw new \BadMethodCallException('Tried to __get inexisting property "'.get_class($this).'->'.$memberName.'"!');
       }
