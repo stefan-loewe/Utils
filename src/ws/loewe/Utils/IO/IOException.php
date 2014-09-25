0eafc67bfeca11005e93e72817ae6260c83e2783
Stefan Löwe <loewe@fim.uni-passau.de>
2014-09-25 21:38:47 +0200

    added class to model IOException

---
+++ src/ws/loewe/Utils/IO/IOException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ws\loewe\Utils\IO;
+
+class IOException extends Exception {
+
+  public function __construct($message, $code = null, $previous = null) {
+    parent::__construct($message, $code, $previous);
+  }
+}
